dfafa20b590cd8f680b4650a8d4797cd8ed249e0
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-02 21:11:12 -0500

    modified

--- js/multiply-divide.js
+++ js/multiply-divide.js
@@ -1,1 +1,9 @@
 "use strict";
+var firstNum = 0;
+var secondNum = 0;
+function multiply() {
+    return firstNum * secondNum;
+}
+function divide() {
+    return firstNum / secondNum;
+}
---
+++ ts/multiply-divide.ts
@@ -0,0 +1,10 @@
+let firstNum = 0;
+let secondNum = 0;
+
+function multiply() {
+   return firstNum * secondNum;
+}
+
+function divide() {
+  return firstNum / secondNum;
+}
