9e40cbeaf99c6a93c35479543ad70bc638ba9374
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-07 21:16:45 -0500

    completed

--- js/multiply-divide.js
+++ js/multiply-divide.js
@@ -1,7 +1,17 @@
 "use strict";
 function multiply() {
-    var num1 = document.getElementById("firstNum");
-    var num2 = document.getElementById("secondNum");
+    var val1 = document.getElementById("firstNum").value;
+    var val2 = document.getElementById("secondNum").value;
+    var result = parseFloat(val1) * parseFloat(val2);
+    updateDisplay(result);
 }
 function divide() {
+    var val1 = document.getElementById("firstNum").value;
+    var val2 = document.getElementById("secondNum").value;
+    var result = parseFloat(val1) / parseFloat(val2);
+    updateDisplay(result);
+}
+function updateDisplay(number) {
+    var input = document.getElementById("numInput");
+    input.value = number.toString();
 }
--- ts/multiply-divide.ts
+++ ts/multiply-divide.ts
@@ -1,12 +1,18 @@
-
-
 function multiply() {
-let num1 = document.getElementById("firstNum") as HTMLInputElement;
-let num2 = document.getElementById("secondNum") as HTMLInputElement;
-
+  let val1 = (document.getElementById("firstNum") as HTMLInputElement).value;
+  let val2 = (document.getElementById("secondNum") as HTMLInputElement).value;
+  let result = parseFloat(val1) * parseFloat(val2);
+  updateDisplay(result);
 }
 
 function divide() {
-  
+  let val1 = (document.getElementById("firstNum") as HTMLInputElement).value;
+  let val2 = (document.getElementById("secondNum") as HTMLInputElement).value;
+  let result = parseFloat(val1) / parseFloat(val2);
+  updateDisplay(result);
 }
 
+function updateDisplay(number: number) {
+  let input = document.getElementById("numInput") as HTMLInputElement;
+  input.value = number.toString();
+}
